b922b86c96d53cb6b0a88508cdb021d5ea5135b6
HanyuHenry <crocodilehy@gmail.com>
2024-12-18 16:13:56 -0700

    early version of the website - local run pass

--- vite-config.js
+++ vite-config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/portfolio-website/', // 替换成你的仓库名
+  base: '/sde-personal_website/'
 })
